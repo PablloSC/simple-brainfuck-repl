37bb9d9d28f7cbe4c2255a02146fb93ad61f94cc
pabllo <pabllosilvacravo@gmail.com>
2022-02-26 14:13:20 -0300

    binary ascci table
    Signed-off-by: pabllo <pabllosilvacravo@gmail.com>

--- interpreter-bf.c
+++ interpreter-bf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 void ascci_table(char *ascci_s);
+char *chartobin (unsigned char c);
 
 int main(int argc, char **argv){
   puts("******");
@@ -19,11 +21,28 @@ int main(int argc, char **argv){
 }
 
 void ascci_table(char *ascci_s){
-      puts    ("+------------------------------+");
-      printf  ("| char | decimal | hexadecimal |\n");
-      puts    ("+------------------------------+");
-      for (int i = 0; i < strlen(ascci_s); i++)
-        printf("| %c    | %-7d | %-11x |\n",
-              ascci_s[i], ascci_s[i], ascci_s[i]);
-      puts    ("+------------------------------+");
+  printf("mensagem: %s\n", ascci_s);
+  puts    ("+-------------------------------------------------+");
+  printf  ("| char |  binary  | octal | decimal | hexadecimal |\n");
+  puts    ("+-------------------------------------------------+");
+  for (int i = 0; i < strlen(ascci_s); i++) {
+    printf("| %-4c | %s | %-5o | %-7d | %-11x |\n",
+        ascci_s[i], chartobin(ascci_s[i]), ascci_s[i], ascci_s[i], ascci_s[i]);
+  }
+  puts    ("+-------------------------------------------------+");
+}
+
+/* thanks Prelude
+ * cboard.cprogramming.com/c-programming/42817-convert-char-binary.html
+ */
+char *chartobin (unsigned char c){
+  static char bin[CHAR_BIT + 1] = {0};
+  int i;
+
+  for (i = CHAR_BIT - 1; i >= 0; i--) {
+    bin[i] = (c % 2) + '0';
+    c /= 2;
+  }
+
+  return bin;
 }
